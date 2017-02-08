ab22d93702c4d75140f9b2f55e82f7c47d7a58ba
Dimitrianos Savva <dimis@di.uoa.gr>
2017-02-08 16:39:56 +0200

    ALLOWED_HOSTS to all

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b$f*7642(t$eqe%+i6e)m&o)17rvyg%_9&0#&##di3(d=fo4t+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
